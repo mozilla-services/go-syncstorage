e8674f553969a82d213353acdf61d1f8273d58c7
Benson Wong <mostlygeek+git@gmail.com>
2016-01-08 15:27:56 -0800

    remove extraneous JSON tags from BSO struct

--- syncstorage/BSO.go
+++ syncstorage/BSO.go
@@ -7,11 +7,11 @@ import (
 
 // ref: https://docs.services.mozilla.com/storage/apis-1.5.html#basic-storage-object
 type BSO struct {
-	Id        string `json:"id"`
-	Modified  int    `json:"modified"`
-	Payload   string `json:"payload"`
-	SortIndex int    `json:"sortindex"`
-	TTL       int    `json:",omitempty"`
+	Id        string
+	Modified  int
+	Payload   string
+	SortIndex int
+	TTL       int
 }
 
 // MarshalJSON builds a custom json blob since there is no way good way of turning the
